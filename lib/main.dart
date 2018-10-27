7705b2282c41a4a14473b94011a7f72ea5bdc5f0
Francisco <frapagis@hotmail.com>
2018-10-27 22:38:16 +0200

    folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,16 @@ void main(){
 }
 
 class MarvelApp extends StatelessWidget {
-  final store = Store<AppState>();
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Marvel Comic',
       theme: new ThemeData(
         primarySwatch: Colors.red,
         primaryColorDark: Colors.red[800],
         accentColor: Colors.orangeAccent,
       ),
-      home: new MyHomePage(title: 'Marvel'),
+      home: new MyHomePage(),
     );
   }
 }
