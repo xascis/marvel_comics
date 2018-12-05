035670cba5f724c44a3e5e7d316340f89ff4c447
Francisco <frapagis@hotmail.com>
2018-12-05 18:03:37 +0100

    refactor home screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -8,66 +8,82 @@ class HomeScreen extends StatelessWidget {
     final bloc = BlocProvider.of<CharacterBloc>(context);
     
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Marvel Comics"),
-      ),
+      appBar: buildAppBar(),
       body: Column(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: TextField(
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 22.0, color: Colors.black),
-              decoration: InputDecoration(
-                labelText: 'Busca a tu Súper-Héroe',
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                ),
-                filled: true,
-              ),
-              onChanged: (text) {print(text);},
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(5.0),
-                    child: ListTile(
-                      onTap: () {},
-                      leading: Icon(Icons.ac_unit),
-                      title: Text(
-                        "Hero name",
-                        style: TextStyle(fontSize: 20.0),
-                        maxLines: 1,
-                      ),
-                      subtitle: Text(
-                        "Hero description",
-                        style: TextStyle(fontSize: 15.0),
-                        maxLines: 3,
-                      ),
-                    ),
-                  ),
-                  Divider(height: 2.0),
-                ],
-              );
-            }
-          ),
-          ),
-          Center(
-            child: Container(
-              padding: EdgeInsets.all(5.0),
-              child: Text(
-                'Data provided by Marvel. © 2018 Marvel',
-                style: TextStyle(fontSize: 10.0, fontStyle: FontStyle.italic),
-              ),
-            ),
-          ),
+          buildSearchBar(),
+          buildCharacterList(),
+          buildAttributeText(),
         ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text("Marvel Comics"),
+    );
+  }
+  Container buildSearchBar() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: TextField(
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 22.0, color: Colors.black),
+        decoration: InputDecoration(
+          labelText: 'Busca a tu Súper-Héroe',
+          labelStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+          filled: true,
+        ),
+        onChanged: (text) {print(text);},
+      ),
+    );
+  }
+  Expanded buildCharacterList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return buildCharacterTile();
+        }
+      ),
+    );
+  }
+  Column buildCharacterTile() {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(5.0),
+          child: ListTile(
+            onTap: () {},
+            leading: Icon(Icons.ac_unit),
+            title: Text(
+              "Hero name",
+              style: TextStyle(fontSize: 20.0),
+              maxLines: 1,
+            ),
+            subtitle: Text(
+              "Hero description",
+              style: TextStyle(fontSize: 15.0),
+              maxLines: 3,
+            ),
+          ),
+        ),
+        Divider(height: 2.0),
+      ],
+    );
+  }
+  Center buildAttributeText() {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(5.0),
+        child: Text(
+          'Data provided by Marvel. © 2018 Marvel',
+          style: TextStyle(fontSize: 10.0, fontStyle: FontStyle.italic),
+        ),
       ),
     );
   }
